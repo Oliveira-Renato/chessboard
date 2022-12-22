e43d435e27b6501973ce2852f22a07f23789fd18
Oliveira-Renato <renato.printf@gmail.com>
2022-12-22 16:47:25 -0300

    data-set configured: #fix3

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro'
 import { Icon, IconifyIcon } from '@iconify/react';
+import { useTable } from "react-table";
 
 
 export function Home() {
@@ -17,41 +18,35 @@ export function Home() {
   }
 
   useEffect(() => {
-    let vChessPiecesBlack: any[] = 
-        ['<iconify-icon class="chess-piece" data-piece-name="rook" data-eixo-x="a" data-eixo-y data-letter icon="game-icons:chess-rook"></iconify-icon>',
-        '<iconify-icon class="chess-piece" data-piece-name="knight" data-eixo-x="b" data-eixo-y data-letter icon="game-icons:chess-knight"></iconify-icon>',
-        '<iconify-icon class="chess-piece" data-piece-name="bishop" data-eixo-x="c" data-eixo-y data-letter icon="game-icons:chess-bishop"></iconify-icon>',
-        '<iconify-icon class="chess-piece" data-piece-name="queen" data-eixo-x="d" data-eixo-y data-letter icon="game-icons:chess-queen"></iconify-icon>',
-        '<iconify-icon class="chess-piece" data-piece-name="king" data-eixo-x="c" data-eixo-y data-letter icon="game-icons:chess-king"></iconify-icon>',
-        '<iconify-icon class="chess-piece" data-piece-name="bishop" data-eixo-x="f" data-eixo-y data-letter icon="game-icons:chess-bishop"></iconify-icon>',
-        '<iconify-icon class="chess-piece" data-piece-name="knight" data-eixo-x="g" data-eixo-y data-letter icon="game-icons:chess-knight"></iconify-icon>',
-        '<iconify-icon class="chess-piece" data-piece-name="rook" data-eixo-x="h" data-eixo-y data-letter icon="game-icons:chess-rook"></iconify-icon>'
-      ],
-        wAlpha:any[] | string = ["", "a", "b", "c", "d", "e", "f", "g", "h"],
-        wChessBoard: any[][][] = [
-          [['<strong>8</strong>']],
-          [['<strong>7</strong>']],
-          [['<strong>6</strong>']],
-          [['<strong>5</strong>']],
-          [['<strong>4</strong>']],
-          [['<strong>3</strong>']],
-          [['<strong>2</strong>']],
-          [['<strong>1</strong>']],[[]]],
-        vTheadTR: any = document.querySelector("thead tr"), vTbody: any = document.querySelector("tbody"),
-        vPawn = '<iconify-icon class="chess-piece pawn" data-piece-name="pawn" data-eixo-x data-eixo-y  icon="game-icons:chess-pawn" ></iconify-icon>';
+    let vChessPiecesBlack: any[]  = [
+          '<iconify-icon class="chess-piece" data-piece-name="rook" data-eixo-x="a" data-eixo-y data-letter icon="game-icons:chess-rook"></iconify-icon>',
+          '<iconify-icon class="chess-piece" data-piece-name="knight" data-eixo-x="b" data-eixo-y data-letter icon="game-icons:chess-knight"></iconify-icon>',
+          '<iconify-icon class="chess-piece" data-piece-name="bishop" data-eixo-x="c" data-eixo-y data-letter icon="game-icons:chess-bishop"></iconify-icon>',
+          '<iconify-icon class="chess-piece" data-piece-name="queen" data-eixo-x="d" data-eixo-y data-letter icon="game-icons:chess-queen"></iconify-icon>',
+          '<iconify-icon class="chess-piece" data-piece-name="king" data-eixo-x="c" data-eixo-y data-letter icon="game-icons:chess-king"></iconify-icon>',
+          '<iconify-icon class="chess-piece" data-piece-name="bishop" data-eixo-x="f" data-eixo-y data-letter icon="game-icons:chess-bishop"></iconify-icon>',
+          '<iconify-icon class="chess-piece" data-piece-name="knight" data-eixo-x="g" data-eixo-y data-letter icon="game-icons:chess-knight"></iconify-icon>',
+          '<iconify-icon class="chess-piece" data-piece-name="rook" data-eixo-x="h" data-eixo-y data-letter icon="game-icons:chess-rook"></iconify-icon>',
+          {
+            pawn : '<iconify-icon class="chess-piece pawn" data-piece-name="pawn" data-eixo-x data-eixo-y  icon="game-icons:chess-pawn" ></iconify-icon>'
+          }
+        ],
+        wAlpha:string[] = ["", "a", "b", "c", "d", "e", "f", "g", "h"],
+        wChessBoard: any[][][] =  [ [[]], [[]], [[]], [[]], [[]], [[]], [[]], [[]], [[]] ],
+        vTheadTR: any = document.querySelector("thead tr"), vTbody: any = document.querySelector("tbody");
 
     //handleFillSquares   
     const handleFillSquares = () => {
       wChessBoard.forEach(function (board, idx) {
-        //if(!wChessBoard[idx]) {
           for (let i = 0; i < 8; i++) {
-            if((idx % 2 === 0 && i % 2 === 1) || (idx % 2 === 1 && i % 2 === 0)) {
-              wChessBoard[idx].push(['<iconify-icon icon="uim:square" data-eixo-x="'+ wAlpha[i + 1] +'" data-eixo-y='+ (8 - idx) +'></iconify-icon>']);
-            } else {
-              wChessBoard[idx].push(['<iconify-icon icon="uim:square-full" data-eixo-x="'+ wAlpha[i + 1] +'"  data-eixo-y='+ (8 - idx) +'></iconify-icon>']);
+            if(wChessBoard[idx][i]) {
+              if((idx % 2 === 0 && i % 2 === 1) || (idx % 2 === 1 && i % 2 === 0)) {
+                wChessBoard[idx].push(['<iconify-icon icon="uim:square" data-eixo-x="'+ wAlpha[i + 1] +'" data-eixo-y='+ (8 - idx) +'></iconify-icon>']);
+              } else {
+                wChessBoard[idx].push(['<iconify-icon icon="uim:square-full" data-eixo-x="'+ wAlpha[i + 1] +'"  data-eixo-y='+ (8 - idx) +'></iconify-icon>']);
+              }
             }
           }
-        //}
       })
     }
     //Body
@@ -59,11 +54,7 @@ export function Home() {
       for (let i = 0; i < wChessBoard.length; i++) {
         let tr = document.createElement("tr");
         for (let j = 0; j < wChessBoard[i].length; j++) {
-          if (i === 8) {
-            tr.innerHTML += `<th>${wAlpha[j]}</th>`;
-          } else {
-            tr.innerHTML += `<td class="chessboard-square">${wChessBoard[i][j]}</td>`;
-          }
+           i === 8 ? tr.innerHTML += `<th>${wAlpha[j]}</th>` : tr.innerHTML += `<td class="chessboard-square">${wChessBoard[i][j]}</td>`;
         }
         vTbody.append(tr);
       }
@@ -73,23 +64,27 @@ export function Home() {
       handleFillSquares()
 
       wChessBoard.forEach(function (board, idx) {
-        if (idx === 0 || idx === 7) {
-          vChessPiecesBlack.forEach(function (chessPiece, index) {
-            wChessBoard[idx][index + 1] = chessPiece.replace('data-eixo-y','data-eixo-y=' + (8 - idx));
-          });
-  
-          if (idx === 7 && (wChessBoard[idx][4] || wChessBoard[idx][5])) {
-            console.log('here', vChessPiecesBlack[4] )
-            wChessBoard[idx][4] = vChessPiecesBlack[4].replace('data-eixo-x="c"','data-eixo-x="d"');
-            wChessBoard[idx][5] = vChessPiecesBlack[3].replace('data-eixo-x="d"','data-eixo-x="e"');
+        const handleChessPieces = {
+          switchKingAndQueen: () => {
+            vChessPiecesBlack.forEach(function (chessPiece, index) {
+              !(typeof chessPiece === "object") && (wChessBoard[idx][index + 1] = chessPiece.replace('data-eixo-y','data-eixo-y=' + (8 - idx)));
+            });
+    
+            if (idx === 7 && (wChessBoard[idx][4] || wChessBoard[idx][5])) {
+              wChessBoard[idx][4] = vChessPiecesBlack[4].replace('data-eixo-x="c"','data-eixo-x="d"');
+              wChessBoard[idx][5] = vChessPiecesBlack[3].replace('data-eixo-x="d"','data-eixo-x="e"');
+            }
+          },
+          placePawns: ()=> {
+            for (let i = 0; i < 8; i++) {
+              let pawn = vChessPiecesBlack[vChessPiecesBlack.length -1].pawn.replace('data-eixo-x data-eixo-y','data-eixo-x="'+ wAlpha[i + 1] +'"  data-eixo-y='+ (8 - idx));
+              wChessBoard[idx][i + 1] = pawn;
+            }
           }
-        } else if (idx === 1 || idx === 6) {
-          for (let i = 0; i < 8; i++) {
-            wChessBoard[idx][i + 1] = [vPawn.replace('data-eixo-x data-eixo-y','data-eixo-x="'+ wAlpha[i + 1] +'"  data-eixo-y='+ (8 - idx))];
-          }
-        } else {
-          
         }
+
+        if (idx === 0 || idx === 7)  handleChessPieces.switchKingAndQueen() 
+        else if (idx === 1 || idx === 6) handleChessPieces.placePawns()
       });
 
       handleChessBody()
