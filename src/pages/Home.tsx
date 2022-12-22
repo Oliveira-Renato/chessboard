0c31ce3284ea6c96fd61939341de55388a76d2b2
Oliveira-Renato <renato.printf@gmail.com>
2022-12-22 14:25:32 -0300

    data-set configured: #fix2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,48 +38,64 @@ export function Home() {
           [['<strong>2</strong>']],
           [['<strong>1</strong>']],[[]]],
         vTheadTR: any = document.querySelector("thead tr"), vTbody: any = document.querySelector("tbody"),
-        vPawn = '<iconify-icon class="chess-piece pawn" data-piece-name="pawn" data-eixo-x data-eixo-y data-letter icon="game-icons:chess-pawn" ></iconify-icon>';
+        vPawn = '<iconify-icon class="chess-piece pawn" data-piece-name="pawn" data-eixo-x data-eixo-y  icon="game-icons:chess-pawn" ></iconify-icon>';
 
-    //ChessBoard
-    wChessBoard.forEach(function (board, idx) {
-      if (idx === 0 || idx === 7) {
-        vChessPiecesBlack.forEach((chessPiece) => {
-          wChessBoard[idx].push(chessPiece.replace('data-eixo-y','data-eixo-y=' + (8 - idx)));// 8(number of pieces) minus the index number
-        });
-
-        if (idx === 7 && (wChessBoard[idx][4] || wChessBoard[idx][5])) {
-          console.log('here', vChessPiecesBlack[4] )
-          wChessBoard[idx][4] = vChessPiecesBlack[4].replace('data-eixo-x="c"','data-eixo-x="d"');
-          wChessBoard[idx][5] = vChessPiecesBlack[3].replace('data-eixo-x="d"','data-eixo-x="e"');
-        }
-      } else if (idx === 1 || idx === 6) {
-        for (let i = 0; i < 8; i++) {
-          wChessBoard[idx].push([vPawn]);
-        }
-      } else {
-        for (let i = 0; i < 8; i++) {
-          if((idx % 2 === 0 && i % 2 === 1) || (idx % 2 === 1 && i % 2 === 0)) {
-            wChessBoard[idx].push(['<iconify-icon icon="uim:square" data-eixo-x="'+ wAlpha[i + 1] +'" data-eixo-y='+ (idx -2 ) +'></iconify-icon>']);
-          } else {
-            wChessBoard[idx].push(['<iconify-icon icon="uim:square-full" data-eixo-x="'+ wAlpha[i + 1] +'"  data-eixo-y='+ (idx -2 ) +'></iconify-icon>']);
+    //handleFillSquares   
+    const handleFillSquares = () => {
+      wChessBoard.forEach(function (board, idx) {
+        //if(!wChessBoard[idx]) {
+          for (let i = 0; i < 8; i++) {
+            if((idx % 2 === 0 && i % 2 === 1) || (idx % 2 === 1 && i % 2 === 0)) {
+              wChessBoard[idx].push(['<iconify-icon icon="uim:square" data-eixo-x="'+ wAlpha[i + 1] +'" data-eixo-y='+ (8 - idx) +'></iconify-icon>']);
+            } else {
+              wChessBoard[idx].push(['<iconify-icon icon="uim:square-full" data-eixo-x="'+ wAlpha[i + 1] +'"  data-eixo-y='+ (8 - idx) +'></iconify-icon>']);
+            }
           }
-         console.log(wChessBoard[i][idx])
-        }
-      }
-    });
-
-    //Body
-    for (let i = 0; i < wChessBoard.length; i++) {
-      let tr = document.createElement("tr");
-      for (let j = 0; j < wChessBoard[i].length; j++) {
-        if (i === 8) {
-          tr.innerHTML += `<th>${wAlpha[j]}</th>`;
-        } else {
-          tr.innerHTML += `<td class="chessboard-square">${wChessBoard[i][j]}</td>`;
-        }
-      }
-      vTbody.append(tr);
+        //}
+      })
     }
+    //Body
+    const handleChessBody = () => {
+      for (let i = 0; i < wChessBoard.length; i++) {
+        let tr = document.createElement("tr");
+        for (let j = 0; j < wChessBoard[i].length; j++) {
+          if (i === 8) {
+            tr.innerHTML += `<th>${wAlpha[j]}</th>`;
+          } else {
+            tr.innerHTML += `<td class="chessboard-square">${wChessBoard[i][j]}</td>`;
+          }
+        }
+        vTbody.append(tr);
+      }
+    }
+    //ChessBoard
+    const handleChessBoard = () => {
+      handleFillSquares()
+
+      wChessBoard.forEach(function (board, idx) {
+        if (idx === 0 || idx === 7) {
+          vChessPiecesBlack.forEach(function (chessPiece, index) {
+            wChessBoard[idx][index + 1] = chessPiece.replace('data-eixo-y','data-eixo-y=' + (8 - idx));
+          });
+  
+          if (idx === 7 && (wChessBoard[idx][4] || wChessBoard[idx][5])) {
+            console.log('here', vChessPiecesBlack[4] )
+            wChessBoard[idx][4] = vChessPiecesBlack[4].replace('data-eixo-x="c"','data-eixo-x="d"');
+            wChessBoard[idx][5] = vChessPiecesBlack[3].replace('data-eixo-x="d"','data-eixo-x="e"');
+          }
+        } else if (idx === 1 || idx === 6) {
+          for (let i = 0; i < 8; i++) {
+            wChessBoard[idx][i + 1] = [vPawn.replace('data-eixo-x data-eixo-y','data-eixo-x="'+ wAlpha[i + 1] +'"  data-eixo-y='+ (8 - idx))];
+          }
+        } else {
+          
+        }
+      });
+
+      handleChessBody()
+    }
+    
+    handleChessBoard()
     hanldeWhiteAndBlack()
   });
 
