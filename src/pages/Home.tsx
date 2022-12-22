4f2ec1bc28243e6c32668b2819d4e02f27af1a4a
Oliveira-Renato <renato.printf@gmail.com>
2022-12-22 12:10:31 -0300

    data-set configured

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,38 +2,56 @@ import React, { useState, useEffect } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid, regular, brands, icon } from '@fortawesome/fontawesome-svg-core/import.macro'
-import { Icon } from '@iconify/react';
+import { Icon, IconifyIcon } from '@iconify/react';
 
 
 export function Home() {
+  const hanldeWhiteAndBlack = () => {
+    let vChessPieces = document.querySelectorAll('.chess-piece')
+
+    for (let index = 0; index < vChessPieces.length ; index++) {
+      if(index < (vChessPieces.length / 2)) {
+        vChessPieces[index].classList.add('chess-piece-white')
+      }
+    }
+  }
+
   useEffect(() => {
     let vChessPiecesBlack: any[] = 
-        ['<iconify-icon class="chess-piece" icon="game-icons:chess-rook"></iconify-icon>',
-        '<iconify-icon class="chess-piece" icon="game-icons:chess-knight"></iconify-icon>',
-        '<iconify-icon class="chess-piece" icon="game-icons:chess-bishop"></iconify-icon>',
-        '<iconify-icon class="chess-piece" icon="game-icons:chess-queen"></iconify-icon>',
-        '<iconify-icon class="chess-piece" icon="game-icons:chess-king"></iconify-icon>',
-        '<iconify-icon class="chess-piece" icon="game-icons:chess-bishop"></iconify-icon>',
-        '<iconify-icon class="chess-piece" icon="game-icons:chess-knight"></iconify-icon>',
-        '<iconify-icon class="chess-piece" icon="game-icons:chess-bishop"></iconify-icon>'
+        ['<iconify-icon class="chess-piece" data-piece-name="rook" data-eixo-x="a" data-eixo-y data-letter icon="game-icons:chess-rook"></iconify-icon>',
+        '<iconify-icon class="chess-piece" data-piece-name="knight" data-eixo-x="b" data-eixo-y data-letter icon="game-icons:chess-knight"></iconify-icon>',
+        '<iconify-icon class="chess-piece" data-piece-name="bishop" data-eixo-x="c" data-eixo-y data-letter icon="game-icons:chess-bishop"></iconify-icon>',
+        '<iconify-icon class="chess-piece" data-piece-name="queen" data-eixo-x="d" data-eixo-y data-letter icon="game-icons:chess-queen"></iconify-icon>',
+        '<iconify-icon class="chess-piece" data-piece-name="king" data-eixo-x="c" data-eixo-y data-letter icon="game-icons:chess-king"></iconify-icon>',
+        '<iconify-icon class="chess-piece" data-piece-name="bishop" data-eixo-x="f" data-eixo-y data-letter icon="game-icons:chess-bishop"></iconify-icon>',
+        '<iconify-icon class="chess-piece" data-piece-name="knight" data-eixo-x="g" data-eixo-y data-letter icon="game-icons:chess-knight"></iconify-icon>',
+        '<iconify-icon class="chess-piece" data-piece-name="rook" data-eixo-x="h" data-eixo-y data-letter icon="game-icons:chess-rook"></iconify-icon>'
       ],
         wAlpha:any[] = ["", "a", "b", "c", "d", "e", "f", "g", "h"],
-        wChessBoard: any[][][] = [[[1]],[[2]],[[3]],[[4]],[[5]],[[6]],[[7]],[[8]],[[]]],
+        wChessBoard: any[][][] = [
+          [['<strong>8</strong>']],
+          [['<strong>7</strong>']],
+          [['<strong>6</strong>']],
+          [['<strong>5</strong>']],
+          [['<strong>4</strong>']],
+          [['<strong>3</strong>']],
+          [['<strong>2</strong>']],
+          [['<strong>1</strong>']],[[]]],
         vTheadTR: any = document.querySelector("thead tr"), vTbody: any = document.querySelector("tbody"),
-        vPawn = '<iconify-icon class="chess-piece" icon="game-icons:chess-pawn"></iconify-icon>';
+        vPawn = '<iconify-icon class="chess-piece pawn" data-piece-name data-eixo-x data-eixo-y data-letter icon="game-icons:chess-pawn" ></iconify-icon>';
 
     //ChessBoard
     wChessBoard.forEach(function (board, idx) {
       if (idx === 0 || idx === 7) {
         vChessPiecesBlack.forEach((chessPiece) => {
-          wChessBoard[idx].push(chessPiece);
+          console.log(chessPiece)
+          wChessBoard[idx].push(chessPiece.replace('data-eixo-y','data-eixo-y=' + (8 - idx)));// 8(number of pieces) minus the index number
         });
 
         if (idx === 7 && (wChessBoard[idx][4] || wChessBoard[idx][5])) {
-          console.log(vChessPiecesBlack[4]);
-          console.log(vChessPiecesBlack[4]);
-          wChessBoard[idx][4] = vChessPiecesBlack[4];
-          wChessBoard[idx][5] = vChessPiecesBlack[3];
+          console.log('here', vChessPiecesBlack[4] )
+          wChessBoard[idx][4] = vChessPiecesBlack[4].replace('data-eixo-x="c"','data-eixo-x="d"');
+          wChessBoard[idx][5] = vChessPiecesBlack[3].replace('data-eixo-x="d"','data-eixo-x="e"');
         }
       } else if (idx === 1 || idx === 6) {
         for (let i = 0; i < 8; i++) {
@@ -50,11 +68,6 @@ export function Home() {
       }
     });
 
-    //Header wAlpha[j]}
-    wChessBoard.forEach(function (item, idx) {
-      vTheadTR.innerHTML += `<th>${idx}</th>`;
-    });
-
     //Body
     for (let i = 0; i < wChessBoard.length; i++) {
       let tr = document.createElement("tr");
@@ -67,6 +80,7 @@ export function Home() {
       }
       vTbody.append(tr);
     }
+    hanldeWhiteAndBlack()
   });
 
   return (
@@ -77,6 +91,7 @@ export function Home() {
           <Icon icon="fa6-solid:chess" />
         </span>
       </div>
+
       <main>
         <div className="chessboard-container">
           <table>
@@ -90,6 +105,7 @@ export function Home() {
           </table>
         </div>
       </main>
+      
     </div>
   )
 }
